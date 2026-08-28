a77f31696773971e5a0f56b207ab86f5910df1ed
Zohaibzahid100 <your-email@example.com>
2026-08-28 16:43:56 +0500

    Updated Class 3

--- Class 3/index.js
+++ Class 3/index.js
@@ -2,46 +2,88 @@ const express = require('express');
 const app = express();
 const port = 3000;
 
-const users = require("./data.json")
-app.use(express.json())
+const users = require("./data.json");
+
+app.use(express.json());
 
 app.get('/', (req, res) => {
   res.send('Hello World!');
 });
 
-// Getting all users
-app.get("/api/users", (req, res)=> {
-    res.json(users)
-})
+app.get("/api/users", (req, res) => {
+    res.json(users);
+});
 
-// Getting single user
-app.get("/api/users/:id", (req,res) => {
-    const id = Number(req.paramas.id)
-    const user = users.find((user) => user.id === id)
-    if(!user){
+app.get("/api/users/:id", (req, res) => {
+    const id = Number(req.params.id);
+    const user = users.find((user) => user.id === id);
+
+    if (!user) {
         return res.status(404).json({
             error: "User not found"
-        })
+        });
     }
-    res.json(user)
-})
 
-// Post method
+    res.json(user);
+});
 
-app.post("/api/users", (req,res) =>{
+app.post("/api/users", (req, res) => {
     const newUser = {
-        id: user.length + 1,
+        id: users.length + 1,
         first_name: req.body.first_name,
         last_name: req.body.last_name,
         email: req.body.email
-    }
+    };
 
-    users.push(newUser)
+    users.push(newUser);
+
     res.status(201).json({
         message: "User Created",
-        user: newUser,
-    })
-})
+        user: newUser
+    });
+    
+});
+
+
+app.put("/api/users/:id", (req, res) => {
+    const id = Number(req.params.id);
+    const user = users.find((user) => user.id === id);
+
+    if (!user) {
+        return res.status(404).json({
+            error: "User not found"
+        });
+    }
+
+    user.first_name = req.body.first_name ;
+    user.last_name = req.body.last_name ;
+    user.email = req.body.email ;
+
+    res.json({
+        message: "User Updated",
+        user: user
+    });
+});
+
+app.delete("/api/users/:id", (req, res) => {
+    const id = Number(req.params.id);
+
+    const userIndex = users.findIndex((user) => user.id === id);
+
+    if (userIndex === -1) {
+        return res.status(404).json({
+            error: "User not found"
+        });
+    }
+
+    const deletedUser = users.splice(userIndex, 1);
+
+    res.json({
+        message: "User deleted",
+        user: deletedUser[0]
+    });
+});
+
 
 app.listen(port, () => {
   console.log(`Server running at http://localhost:${port}`);
