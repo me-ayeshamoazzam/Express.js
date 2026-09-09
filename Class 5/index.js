877b7e59517b1a93cb6822cf9bd5be255ccf8ac1
Zohaibzahid100 <your-email@example.com>
2026-09-09 16:20:04 +0500

    Added Database connected

---
+++ Class 5/index.js
@@ -0,0 +1,18 @@
+const express = require("express")
+const app = express()
+const mongoose = require("mongoose")
+require("dotenv").config()
+app.use(express.json())
+
+mongoose.connect(process.env.MONGO_URI).then(() => {
+  console.log("Mongodb is connected")
+  app.listen(process.env.PORT, () =>{
+    console.log(`Example app listening on port ${process.env.PORT}`)
+  })
+}).catch((err) =>{
+    console.error("Error", err)
+})
+
+app.get("/", (req, res) =>{
+    res.send("Hello world")
+})
